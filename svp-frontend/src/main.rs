7f2ac75cfaf006453913fee97dc7eede15113c87
Chichri <farnezelameyer@gmail.com>
2024-03-06 01:52:56 -0800

    working login UI

--- svp-frontend/src/main.rs
+++ svp-frontend/src/main.rs
@@ -1,8 +1,6 @@
-// old main.rs for the counter functionality using the tui module
-
 use std::io;
 
-use crossterm::event::{self, Event, KeyEvent, KeyModifiers, KeyCode, KeyEventKind};
+use crossterm::event::{self, Event, KeyEvent, KeyModifiers, /*KeyCode,*/ KeyEventKind};
 
 use ratatui::{
     prelude::*,
@@ -14,42 +12,41 @@ use tui_prompts::prelude::*;
 
 mod tui;
 
+//Field holds the current focused field
 #[derive(Debug, Default)]
 enum Field {
     #[default]
+    Server,
     Username,
     Password,
-    Invisible,
 }
 
+//App holds all fields + some other stuff
 #[derive(Debug, Default)]
 pub struct App<'a> {
-    counter: i32,
     exit: bool,
     
-    current_field: Field,
-    invisible_state:   TextState<'a>,
-    username_state: TextState<'a>,
-    password_state: TextState<'a>,
+    current_field:   Field,
+    server_state: TextState<'a>,
+    username_state:  TextState<'a>,
+    password_state:  TextState<'a>,
 }
 
+//implementation of App
 impl <'a>App<'a> {
     /// runs the application's main loop until the user quits
-
+    
+    //main drawing loop
     pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
         *self.current_state().focus_state_mut() = FocusState::Focused;
         while !self.exit {
-            //terminal.draw(|frame| self.render_frame(frame))?;
             terminal.draw(|frame| self.draw_ui(frame))?;
             self.handle_events()?;
         }
         Ok(())
     }
-
-    fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
-    }
-
+    
+    //event handling loop
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
             // it's important to check that the event is a key press event as
@@ -61,14 +58,13 @@ impl <'a>App<'a> {
         };
         Ok(())
     }
-
+    
+    //handling all KeyEvents
     fn handle_key_event(&mut self, key_event: KeyEvent) {
 
         match (key_event.code, key_event.modifiers) {
 
-            (event::KeyCode::Left , _)=> self.decrement_counter(),
-            (event::KeyCode::Right , _)=> self.increment_counter(),
-            (event::KeyCode::Char('q'), _)=> self.exit(),
+            (event::KeyCode::Char('c'), KeyModifiers::CONTROL)=> self.exit(),
             (event::KeyCode::Enter, _) => self.submit(),
             (event::KeyCode::Tab, KeyModifiers::NONE) => self.focus_next(),
             (event::KeyCode::BackTab, KeyModifiers::SHIFT) => self.focus_prev(),
@@ -76,59 +72,83 @@ impl <'a>App<'a> {
         }
 
     }
-
+    
+    //kill the program on q
     fn exit(&mut self) {
         self.exit = true;
     }
 
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
-    }
-
-
+    //Main UI drawing function, called in run() 
     fn draw_ui(&mut self, frame: &mut Frame) {
-        let (username_area, password_area, invisible_area, value_area, debug_area) =
-            self.split_layout(frame.size());
+
+        let (username_area, password_area, server_area, value_area, _debug_area) = self.arrange_login(frame.size());
+
+        let title = Title::from(" Secure Virtual Pets ".bold());
+        let quit_instruction = Title::from(" Ctrl C to quit ");
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(quit_instruction.alignment(Alignment::Center).position(Position::Bottom))
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+
+
+        frame.render_widget(block, frame.size());
+
         self.draw_text_prompt(frame, username_area);
         self.draw_password_prompt(frame, password_area);
-        self.draw_invisible_prompt(frame, invisible_area);
+        self.draw_server_prompt(frame, server_area);
         self.draw_state_value(frame, value_area);
-        //self.draw_debug(frame, debug_area);
-        //
+        //self.draw_debug(frame, debug_area);    //keep commented unless you just want to see the state of the program
 
     }
 
-    fn split_layout(&self, area: Rect) -> (Rect, Rect, Rect, Rect, Rect) {
-        let (prompt_area, debug_area) = if false {
-            let areas = Layout::default()
-                .direction(Direction::Horizontal)
-                .constraints(vec![Constraint::Ratio(1, 2); 2])
-                .split(area);
-            (areas[0], areas[1])
-        } else {
-            (area, area)
-        };
-        let areas = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints(vec![Constraint::Length(1); 4])
-            .split(prompt_area);
-        (areas[0], areas[1], areas[2], areas[3], debug_area)
+    //arrange the areas of the text prompts
+    fn arrange_login(&self, area: Rect) -> (Rect, Rect, Rect, Rect, Rect) {
+        let server_area  = Rect::new(88, 20, area.width, 1);
+        let username_area  = Rect::new(88, 21, area.width, 1);
+        let password_area = Rect::new(88, 22, area.width, 1);
+        let value_area     = Rect::new(88, 23, area.width, 1);
+        let debug_area     = Rect::new(88, 24, area.width, 1);
+
+        (username_area, password_area, server_area, value_area, debug_area)
     }
 
-
+    //matching the current state to the app state
     fn current_state(&mut self) -> &mut TextState<'a> {
         match self.current_field {
+            Field::Server => &mut self.server_state,
             Field::Username => &mut self.username_state,
             Field::Password => &mut self.password_state,
-            Field::Invisible => &mut self.invisible_state,
         }
     }
+    
+    // draw the value of the current state underneath the prompts for debugging purposes
+    fn draw_state_value(&mut self, frame: &mut Frame, value_area: Rect) {
+        let state = self.current_state();
+        let state = format!("  Value: {}", state.value());
+        frame.render_widget(
+            Paragraph::new(state).style(Style::new().dark_gray()),
+            value_area,
+        );
+    }
 
+    // draw a debug string in the top right corner of the screen that shows the current state of
+    // the app.
+    // commented out to avoid the warning
+    //fn draw_debug(&mut self, frame: &mut Frame, area: Rect) {
+        //if false {
+            //return;
+        //}
+        //let debug = format!("{self:#?}");
+        //frame.render_widget(
+            //Paragraph::new(debug)
+                //.wrap(Wrap { trim: false })
+                //.block(Block::new().borders(Borders::LEFT)),
+            //area,
+        //);
+    //}
 
+    // Drawing the prompts
     fn draw_text_prompt(&mut self, frame: &mut Frame, username_area: Rect) {
         TextPrompt::from("Username").draw(frame, username_area, &mut self.username_state);
     }
@@ -139,38 +159,12 @@ impl <'a>App<'a> {
             .draw(frame, password_area, &mut self.password_state);
     }
 
-    fn draw_invisible_prompt(&mut self, frame: &mut Frame, invisible_area: Rect) {
-        TextPrompt::from("Invisible")
-            .with_render_style(TextRenderStyle::Invisible)
-            .draw(frame, invisible_area, &mut self.invisible_state);
-    }
-
-    /// draw the value of the current state underneath the prompts.
-    fn draw_state_value(&mut self, frame: &mut Frame, value_area: Rect) {
-        let state = self.current_state();
-        let state = format!("  Value: {}", state.value());
-        frame.render_widget(
-            Paragraph::new(state).style(Style::new().dark_gray()),
-            value_area,
-        );
-    }
-
-    /// draw a debug string in the top right corner of the screen that shows the current state of
-    /// the app.
-    fn draw_debug(&mut self, frame: &mut Frame, area: Rect) {
-        if false {
-            return;
-        }
-        let debug = format!("{self:#?}");
-        frame.render_widget(
-            Paragraph::new(debug)
-                .wrap(Wrap { trim: false })
-                .block(Block::new().borders(Borders::LEFT)),
-            area,
-        );
+    fn draw_server_prompt(&mut self, frame: &mut Frame, server_area: Rect) {
+        TextPrompt::from("Server").draw(frame, server_area, &mut self.server_state);
     }
 
 
+    //focusing between states
     fn focus_handle_event(&mut self, key_event: KeyEvent) {
         let state = self.current_state();
         state.handle_key_event(key_event);
@@ -187,7 +181,25 @@ impl <'a>App<'a> {
         self.current_field = self.prev_field();
         self.current_state().focus();
     }
+    
+    //swtiching between states
+    fn next_field(&mut self) -> Field {
+        match self.current_field {
+            Field::Username => Field::Password,
+            Field::Password => Field::Server,
+            Field::Server => Field::Username,
+        }
+    }
 
+    fn prev_field(&mut self) -> Field {
+        match self.current_field {
+            Field::Username => Field::Server,
+            Field::Password => Field::Username,
+            Field::Server => Field::Password,
+        }
+    }
+    
+    //submitting a string to a state
     fn submit(&mut self) {
         self.current_state().complete();
         if self.current_state().is_finished() && !self.is_finished() {
@@ -197,67 +209,17 @@ impl <'a>App<'a> {
         }
     }
 
-    fn next_field(&mut self) -> Field {
-        match self.current_field {
-            Field::Username => Field::Password,
-            Field::Password => Field::Invisible,
-            Field::Invisible => Field::Username,
-        }
-    }
-
-    fn prev_field(&mut self) -> Field {
-        match self.current_field {
-            Field::Username => Field::Invisible,
-            Field::Password => Field::Username,
-            Field::Invisible => Field::Password,
-        }
-    }
-
+    //when all states have been finished
     fn is_finished(&self) -> bool {
         self.username_state.is_finished()
             && self.password_state.is_finished()
-            && self.invisible_state.is_finished()
+            && self.server_state.is_finished()
     }
 
 
 }
 
-impl <'a>Widget for &App<'a> {
-
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
-
-
-}
-
+//testing basic functionality
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -307,6 +269,7 @@ mod tests {
 
 }
 
+//main function
 fn main() -> io::Result<()> {
 
     let mut terminal = tui::init()?;
